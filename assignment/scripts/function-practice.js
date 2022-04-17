1cbdebbc24af342c041dd0dc32e0f5e821bfa3c3
Philippe <kamokamophilippe13@gmail.com>
2022-04-16 19:34:47 -0500

    complete

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,31 +15,43 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  name = 'Snel';
+  return name;
 }
+console.log('Hello', helloName());
 // Remember to call the function to test
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber, secondNumber ) {
   // return firstNumber + secondNumber;
+  let answer = firstNumber + secondNumber;
+  return answer;
+  
 }
+console.log(addNumbers(3, 5));
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
+function multiplyThree( num1, num2, num3 ){
+  let answer = num1 * num2 * num3;
+  return answer;
 
-}
+} 
+console.log(multiplyThree(2, 5, 4));
 
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return true;
+  } else{ 
+    return false;
   }
-    return;
+    
 }
+
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
 console.log( 'isPositive - should say true', isPositive(3) );
@@ -49,41 +61,97 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
+let stars = ['ball', 'bike', 'car', 'book'];
 function getLast( array ) {
+  if (array = stars[stars.length-1]){
+    return array;
+  } else {
+    return undefined;
+  }
 
 }
-
+console.log('last item is', getLast());
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
-function find( value, array ){
+let a2 = ['bus', 'car', 'moto'];
+function findValue(){
+  for(let item of a2) {
+    if(item === 'car' || item ==='bus' || item==='moto'){
+       return true;
+    }
+    else {
+    return false;
+    }
   
-}
+  }
+  
+  }
+
+  console.log('This is the value is', findValue());
 
 // ----------------------
 // Stretch Goals
-// ----------------------
-// 8. Function to check if a letter is the first letter in a 
-//    string. Return true if it is, and false otherwise
+ //----------------------
+ //8. Function to check if a letter is the first letter in a 
+   // string. Return true if it is, and false otherwise
+// let letter =['apple', 'zelle'];
+let letter ='';
 function isFirstLetter(letter, string) {
+  if(letter.charAt(0)=== 'a'){
+    return true;
+  }
+  return false;
+  
+  }
 
-}
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
-  let sum = 0
-  // TODO: loop to add items
+let arrayNum = [10, 20, 30];
+let sum = 0;
+function sumAll() {
+  for(let i=0; i< arrayNum.length; i++){
+
+  
+  sum +=arrayNum[i];
+  }
   return sum;
+
 }
+console.log(sumAll());
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 
-
+let arrayZ =[2, 6, -6, -3];
+function greaterZero(){
+  for(let i=0; i<arrayZ.length; i++){
+    if(arrayZ[i] > 0){
+    console.log(arrayZ[i] + '');
+    }
+  }
+}
+console.log(greaterZero());
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
+
+//Given two numbers, return true if the sum 
+// of both numbers is less than 100. Otherwise return false.
+
+function numberBoth(a, b){
+  if(a + b < 100) {
+    return true;
+  } else {
+    return false;
+  }
+}  
+console.log(numberBoth(544, 30));
+console.log(numberBoth(59, 34));
+console.log(numberBoth(45, 390));
+console.log(numberBoth(50, 78));
+console.log(numberBoth(54, 122));
